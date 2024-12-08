3dd33bc0b14cc900c502766dc35847b873635962
OmarAlex24 <artcate208@gmail.com>
2024-12-08 00:09:19 -0600

    Eliminado el campo 'codigo' de la clase Vuelo y actualizado el constructor correspondiente.

--- BoletosYaBackend/src/main/java/com/omar/entity/Vuelo.java
+++ BoletosYaBackend/src/main/java/com/omar/entity/Vuelo.java
@@ -1,6 +1,7 @@
 package com.omar.entity;
 
 import lombok.Data;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,7 +9,6 @@ import java.util.List;
 @Data
 public class Vuelo {
     private int id;
-    private String codigo;
     private Aerolinea aerolinea;
     private Aeropuerto origen;
     private Aeropuerto destino;
@@ -20,10 +20,9 @@ public class Vuelo {
     public Vuelo() {
     }
 
-    public Vuelo(Aerolinea aerolinea,String codigo, Aeropuerto origen, Aeropuerto destino, LocalDateTime fechaSalida, LocalDateTime fechaLlegada, double precio) {
+    public Vuelo(Aerolinea aerolinea, Aeropuerto origen, Aeropuerto destino, LocalDateTime fechaSalida, LocalDateTime fechaLlegada, double precio) {
         this.aerolinea = aerolinea;
         this.origen = origen;
-        this.codigo = codigo;
         this.destino = destino;
         this.fechaSalida = fechaSalida;
         this.fechaLlegada = fechaLlegada;
